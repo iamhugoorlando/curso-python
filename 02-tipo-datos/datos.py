0ccd5bc294385ab42a8984ea9ed42e557565b797
iamhugoorlando <hugo.orlando2030@gmail.com>
2020-03-02 13:36:48 -0300

    update tipo de datos

---
+++ 02-tipo-datos/datos.py
@@ -0,0 +1,21 @@
+# Aprenderemos sobre tipo de datos en Python
+
+# Todos los valores de Python tienen un tipo de dato
+# Los tipos de datos basicos son:
+# Integer <int>
+# Float <float>
+# String <str>
+# Boolean <boo>
+
+# Con la funcion type() podremos saber que tipo de dato es nuestro valores
+
+print(type('Hola Mundo'))
+print(type(2))
+print(type(2.9))
+print(type(True))
+
+print('Hola Mundo')
+print(2)
+print(2.9)
+print(True)
+print(False)
