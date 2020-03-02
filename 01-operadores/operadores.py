b6759b474df472747768c393979ddfcda724f5bf
iamhugoorlando <hugo.orlando2030@gmail.com>
2020-03-02 13:34:41 -0300

    update folder

---
+++ 01-operadores/operadores.py
@@ -0,0 +1,18 @@
+# Aprenderemos sobre operadores matematicos en Python
+
+# Nos permiten trabajar con valores y generar los mismos
+# Nos permiten evaluar expresiones en el lenguaje
+# Los operadores son:
+# suma +, resta -, multi *, div /, modulo %
+# potencia **, mayor >, menor <, igual ==
+
+a = 2
+b = 2
+print(a+b) # sumamos a + b
+print(a*b) # multiplicamos a * b
+print(a/b) # dividimos a / b
+print(a%b) # modulo de a entre b
+print(a>b) # a mayor que b
+print(a<b) # menor a menor que b
+print(a==b) # a igual b
+print(a**b) # a elevada a la potencia b
